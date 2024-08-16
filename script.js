0f6f14a138e35374aaec55e6d00ff0089dcb9659
Juan <joaopbg06@gmail.com>
2024-08-16 16:55:33 -0300

    miss text style

--- script.js
+++ script.js
@@ -51,3 +51,18 @@ function autoAvancar() {
 // setInterval(autoAvancar, 3000);
 
 carregar();
+
+
+const videoUrl = "https://www.youtube.com/embed/Y0fPZYECVgs";
+        
+// Seleciona o elemento com a classe 'video'
+const videoDiv = document.querySelector('.video');
+
+// Cria um iframe para incorporar o vídeo
+const iframe = document.createElement('iframe');
+iframe.src = videoUrl;
+iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+iframe.allowFullscreen = true;
+
+// Adiciona o iframe dentro da div com a classe 'video'
+videoDiv.appendChild(iframe);
